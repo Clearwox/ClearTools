1273f99fa38266d7964eca172041235f0378f909
Godwin Ehichoya <ehgodson@hotmail.com>
2025-06-30 07:21:42 +0100

    Added service extenmsion for automatically adding services by common interface
    - Bump version in `ClearTools.csproj` from 3.0.5 to 3.0.6.
    - Introduce `ServiceCollectionExtensions.cs` with extension methods for `IServiceCollection`.
    - Add `ServiceLifetimeOption` enum for service lifetime management.
    - Implement methods to register services by interface with specified lifetimes: `AddSingletonServicesByInterface<T>`, `AddScopedServicesByInterface<T>`, and `AddTransientServicesByInterface<T>`.
    - Include helper methods for retrieving exported types from assemblies.

---
+++ src/ClearTools/extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace ClearTools.extensions
+{
+    enum ServiceLifetimeOption
+    {
+        Singleton,
+        Scoped,
+        Transient
+    }
+
+    public static class ServiceCollectionExtensions
+
+    {
+        public static IServiceCollection AddSingletonServicesByInterface<T>(this IServiceCollection services, Assembly? assembly = null) where T : class
+        {
+            return AddServicesByInterface<T>(services, ServiceLifetimeOption.Singleton, assembly);
+        }
+
+        public static IServiceCollection AddScopedServicesByInterface<T>(this IServiceCollection services, Assembly? assembly = null) where T : class
+        {
+            return AddServicesByInterface<T>(services, ServiceLifetimeOption.Scoped, assembly);
+        }
+
+        public static IServiceCollection AddTransientServicesByInterface<T>(this IServiceCollection services, Assembly? assembly = null) where T : class
+        {
+            return AddServicesByInterface<T>(services, ServiceLifetimeOption.Transient, assembly);
+        }
+
+        private static IServiceCollection AddServicesByInterface<T>(this IServiceCollection services, ServiceLifetimeOption lifetime, Assembly? assembly = null) where T : class
+        {
+            var serviceTypes = GetAllExportedTypes(assembly)
+                .Where(t => t.IsClass && !t.IsAbstract && typeof(T).IsAssignableFrom(t));
+
+            foreach (var serviceType in serviceTypes)
+            {
+                var interfaceType = serviceType.GetInterfaces().FirstOrDefault(i => typeof(T).IsAssignableFrom(i));
+                if (interfaceType != null)
+                {
+                    switch (lifetime)
+                    {
+                        case ServiceLifetimeOption.Singleton:
+                            services.AddSingleton(interfaceType, serviceType);
+                            break;
+                        case ServiceLifetimeOption.Scoped:
+                            services.AddScoped(interfaceType, serviceType);
+                            break;
+                        case ServiceLifetimeOption.Transient:
+                            services.AddTransient(interfaceType, serviceType);
+                            break;
+                        default:
+                            throw new ArgumentOutOfRangeException(nameof(lifetime), lifetime, null);
+                    }
+
+                    services.AddScoped(interfaceType, serviceType);
+                }
+            }
+
+            return services;
+        }
+
+        private static IEnumerable<Type> GetAllExportedTypes(Assembly? assembly = null)
+        {
+            return assembly == null
+                ? AppDomain.CurrentDomain.GetAssemblies().SelectMany(a => a.ExportedTypes)
+                : assembly.ExportedTypes;
+        }
+    }
+}
